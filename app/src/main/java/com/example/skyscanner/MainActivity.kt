d0e371162d443c20107a74b5411d0598f71f285b
Askary Abbas <dev.askary@gmail.com>
2025-11-06 18:06:24 -0500

    added activity file and updated the design.

--- app/src/main/java/com/example/skyscanner/MainActivity.kt
+++ app/src/main/java/com/example/skyscanner/MainActivity.kt
@@ -2,11 +2,6 @@ package com.example.skyscanner
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -16,17 +11,7 @@ import com.example.skyscanner.ui.theme.SkyscannerTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            SkyscannerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
     }
 }
 
